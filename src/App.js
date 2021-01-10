869594a6dc67682f9e796ce4446f3cc60204e652
jakob <jakmanne@gmail.com>
2021-01-10 16:03:25 +0100

    feat: remove ajax

--- src/App.js
+++ src/App.js
@@ -25,18 +25,9 @@ class App extends Component {
   }
 
   getResumeData(){
-    $.ajax({
-      url:'/resumeData.json',
-      dataType:'json',
-      cache: false,
-      success: function(data){
-        this.setState({resumeData: data});
-      }.bind(this),
-      error: function(xhr, status, err){
-        console.log(err);
-        alert(err);
-      }
-    });
+
+    let data = require('./resumeData.json');
+    this.setState({resumeData: data});
   }
 
   componentDidMount(){
