367e05b7b26b2dd0b205377d77b9f2e9b45196ea
jakob <jakmanne@gmail.com>
2021-01-10 20:52:02 +0100

    changed header bar

--- src/App.js
+++ src/App.js
@@ -40,7 +40,7 @@ class App extends Component {
         <Header data={this.state.resumeData.main}/>
         <About data={this.state.resumeData.main}/>
         <Resume data={this.state.resumeData.resume}/>
-        <Portfolio data={this.state.resumeData.portfolio}/>  
+        <Portfolio data={this.state.resumeData.portfolio}/>
         <Footer data={this.state.resumeData.main}/>
       </div>
     );
