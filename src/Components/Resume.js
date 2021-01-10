97368210718689b73bb04f5be5d0cc1d313350fa
jakob <jakmanne@gmail.com>
2021-01-10 20:23:27 +0100

    added more info about me

--- src/Components/Resume.js
+++ src/Components/Resume.js
@@ -25,33 +25,30 @@ class Resume extends Component {
     return (
       <section id="resume">
 
-      <div className="row education">
-         <div className="three columns header-col">
-            <h1><span>Education</span></h1>
-         </div>
 
-         <div className="nine columns main-col">
-            <div className="row item">
-               <div className="twelve columns">
-                 {education}
-               </div>
-            </div>
-         </div>
-      </div>
+        <div className="row work">
 
-
-      <div className="row work">
-
-         <div className="three columns header-col">
+          <div className="three columns header-col">
             <h1><span>Work</span></h1>
-         </div>
+          </div>
 
-         <div className="nine columns main-col">
-          {work}
+          <div className="nine columns main-col">
+            {work}
+          </div>
         </div>
-    </div>
+        <div className="row education">
+          <div className="three columns header-col">
+            <h1><span>Education</span></h1>
+          </div>
 
-
+          <div className="nine columns main-col">
+            <div className="row item">
+              <div className="twelve columns">
+                {education}
+              </div>
+            </div>
+          </div>
+        </div>
 
       <div className="row skill">
 
